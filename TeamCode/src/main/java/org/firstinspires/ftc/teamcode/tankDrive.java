f2dafdb7f6e484c3f2fe221f673521b82b9233bc
MinecraftUsername <aidan.li.2011@gmail.com>
2022-01-15 11:46:51 -0500

    seth's drive modifications

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tankDrive.java
@@ -12,8 +12,9 @@ import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Real Tank Drive")
 public class tankDrive extends OpMode{
-
+    //clock
     private ElapsedTime runtime = new ElapsedTime();
+    //defines motor variables
     private DcMotor leftFront = null;
     private DcMotor leftBack = null;
     private DcMotor rightFront = null;
@@ -22,13 +23,13 @@ public class tankDrive extends OpMode{
     public void init() {
 
         telemetry.addData("Starting robot", "Skynet override success");
-
+        //defines motors as motors
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
         leftBack = hardwareMap.get(DcMotor.class, "leftBack");
         rightBack = hardwareMap.get(DcMotor.class, "rightBack");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
 
-
+        //defines a front
         leftFront.setDirection(DcMotor.Direction.FORWARD);
         leftBack.setDirection(DcMotor.Direction.FORWARD);
         rightFront.setDirection(DcMotor.Direction.REVERSE);
@@ -47,22 +48,34 @@ public class tankDrive extends OpMode{
     }
     @Override
     public void loop() {
-
+        //defining a power variable
         double leftPower;
         double rightPower;
-
+        //get inputs from bumpers
+        boolean leftBumper = gamepad1.left_bumper;
+        boolean rightBumper = gamepad1.right_bumper;
+        //get inputs from stick
         double leftDrive = gamepad1.left_stick_y;
         double rightDrive = gamepad1.right_stick_y;
 
+        //bumpers override the sticks if they are pressed
+        if(leftBumper == true){
+            leftDrive = 1;
+        }
+        if(rightBumper == true){
+            rightDrive = 1;
+        }
+
+        //sets power level
         leftPower = Range.clip(leftDrive, -1.0, 1.0);
         rightPower = Range.clip(rightDrive, -1.0, 1.0);
-
+        //gives power to the wheels
         leftFront.setPower(leftPower);
         leftBack.setPower(leftPower);
         rightFront.setPower(rightPower);
         rightBack.setPower(rightPower);
 
-
+        //data
         telemetry.addData("Time spent fleeing:", " " + runtime.toString());
         telemetry.addData("Stats", "");
         telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
