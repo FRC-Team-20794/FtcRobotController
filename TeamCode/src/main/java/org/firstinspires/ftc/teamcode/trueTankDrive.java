788f3610c66f9eca5586314a8e7ea8138f361bdf
MinecraftUsername <aidan.li.2011@gmail.com>
2022-01-19 17:27:50 -0500

    Added table spinner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trueTankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trueTankDrive.java
@@ -12,17 +12,20 @@ public class trueTankDrive extends OpMode{
     //defines motor variables
     private DcMotor left = null;
     private DcMotor right = null;
+    private DcMotor table = null;
     @Override
     public void init() {
 
         telemetry.addData("Starting robot", "Skynet override success");
         //defines motors as motors
-        left = hardwareMap.get(DcMotor.class, "leftFront");
-        right = hardwareMap.get(DcMotor.class, "rightBack");
+        left = hardwareMap.get(DcMotor.class, "left");
+        right = hardwareMap.get(DcMotor.class, "right");
+        table = hardwareMap.get(DcMotor.class, "turnTable");
 
         //defines a front
         left.setDirection(DcMotor.Direction.FORWARD);
         right.setDirection(DcMotor.Direction.REVERSE);
+        table.setDirection(DcMotor.Direction.FORWARD);
 
         telemetry.addData("Skynet Initialized", "May your last moments be unhappy");
 
@@ -40,18 +43,27 @@ public class trueTankDrive extends OpMode{
         //defining a power variable
         double leftPower;
         double rightPower;
+        double tablePower;
         //get inputs from bumpers
         boolean leftBumper = gamepad1.left_bumper;
         boolean rightBumper = gamepad1.right_bumper;
+        boolean yButton = gamepad1.y;
         //get inputs from stick
         double leftDrive = gamepad1.left_stick_y;
         double rightDrive = gamepad1.right_stick_y;
+
         //bumpers override the sticks if they are pressed
         if(leftBumper){
             leftDrive = 1;
         }
         if(rightBumper){
             rightDrive = 1;
+        }
+        if(yButton){
+            tablePower = 1;
+        }
+        else{
+            tablePower = 0;
         }
 
         //sets power level
@@ -61,6 +73,7 @@ public class trueTankDrive extends OpMode{
         //gives power to the wheels
         left.setPower(leftPower);
         right.setPower(rightPower);
+        table.setPower(tablePower);
         //data
         telemetry.addData("Time spent fleeing:", " " + runtime.toString());
         telemetry.addData("Stats", "");
