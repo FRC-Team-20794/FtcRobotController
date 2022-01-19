9d7adadc72645d75e4b860bebcfd768142ae39d3
MinecraftUsername <aidan.li.2011@gmail.com>
2022-01-19 15:27:54 -0500

    Added the turntable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tankDrive.java
@@ -1,11 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
-
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -19,6 +17,7 @@ public class tankDrive extends OpMode{
     private DcMotor leftBack = null;
     private DcMotor rightFront = null;
     private DcMotor rightBack = null;
+    private CRServo turnTable = null;
     @Override
     public void init() {
 
@@ -28,6 +27,7 @@ public class tankDrive extends OpMode{
         leftBack = hardwareMap.get(DcMotor.class, "leftBack");
         rightBack = hardwareMap.get(DcMotor.class, "rightBack");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        turnTable = hardwareMap.get(CRServo.class, "turnTable");
 
         //defines a front
         leftFront.setDirection(DcMotor.Direction.FORWARD);
@@ -51,30 +51,38 @@ public class tankDrive extends OpMode{
         //defining a power variable
         double leftPower;
         double rightPower;
+        double tablePower;
         //get inputs from bumpers
         boolean leftBumper = gamepad1.left_bumper;
         boolean rightBumper = gamepad1.right_bumper;
+        boolean ducks = gamepad1.y;
         //get inputs from stick
         double leftDrive = gamepad1.left_stick_y;
         double rightDrive = gamepad1.right_stick_y;
 
         //bumpers override the sticks if they are pressed
-        if(leftBumper == true){
+        if(leftBumper){
             leftDrive = 1;
         }
-        if(rightBumper == true){
+        if(rightBumper){
             rightDrive = 1;
         }
-
+        if(ducks){
+            tablePower = 1;
+        }
+        else{
+            tablePower = 0;
+        }
         //sets power level
         leftPower = Range.clip(leftDrive, -1.0, 1.0);
         rightPower = Range.clip(rightDrive, -1.0, 1.0);
+
         //gives power to the wheels
         leftFront.setPower(leftPower);
         leftBack.setPower(leftPower);
         rightFront.setPower(rightPower);
         rightBack.setPower(rightPower);
-
+        turnTable.setPower(tablePower);
         //data
         telemetry.addData("Time spent fleeing:", " " + runtime.toString());
         telemetry.addData("Stats", "");
