856f47b4b868b1dcb6aae170eb29b7bfa7f091f8
SleepySleepy0787 <vmorris@nextcharterschool.org>
2022-01-25 14:04:21 -0500

    Basic mecanum drive mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumDrive.java
@@ -1,9 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "Mecanum Drive", group = "mecanum drive")
 
@@ -14,7 +13,7 @@ public class mecanumDrive extends OpMode{
     private DcMotor backLeft = null;
     private DcMotor backRight = null;
 
-    public void init(){
+    public void init() {
 
         telemetry.addData("Initiating startup", " ");
 
@@ -32,7 +31,40 @@ public class mecanumDrive extends OpMode{
 
         telemetry.addData("Motors online", " ");
 
+    }
 
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start(){
+
+    }
+
+    @Override
+    public void loop(){
+
+        //defined variables
+        double backSlash;
+        double forwardSlash;
+
+        //getting stick inputs
+        backSlash = (-1*(gamepad1.left_stick_y)) + gamepad1.left_stick_x;
+        forwardSlash = gamepad1.left_stick_y + (-1*(gamepad1.left_stick_x));
+
+        frontLeft.setPower(backSlash);
+        frontRight.setPower(forwardSlash);
+        backLeft.setPower(forwardSlash);
+        backRight.setPower(backSlash);
+
+        telemetry.addData("Startup complete", " ");
+
+        }
+
+    @Override
+    public void stop(){
 
     }
 }
