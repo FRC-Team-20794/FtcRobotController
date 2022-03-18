e74d5d82f4b48473605fff815c2d889703095635
MinecraftUsername <aidan.li.2011@gmail.com>
2022-03-17 20:51:44 -0700

    backup code is now in usable state, still need to add drift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aidanMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aidanMecanumDrive.java
@@ -3,7 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name = "Mecanum Drive", group = "mecanum drive")
 
@@ -23,8 +25,8 @@ public class aidanMecanumDrive extends OpMode{
         backLeft = hardwareMap.get(DcMotor.class,"backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
 
-        frontLeft.setDirection(DcMotor.Direction.FORWARD);
-        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
         backLeft.setDirection(DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.REVERSE);
 
@@ -40,7 +42,7 @@ public class aidanMecanumDrive extends OpMode{
 
     }
 
-    public void loop(){
+    public void loop() {
         double frontLeftPower;
         double frontRightPower;
         double backLeftPower;
@@ -48,16 +50,56 @@ public class aidanMecanumDrive extends OpMode{
 
         double x = gamepad1.left_stick_x;
         double y = gamepad1.left_stick_y;
-
+        boolean quickCount = gamepad1.left_bumper;
+        boolean quickClock = gamepad1.right_bumper;
         //motor 1 is x+y, motor 2 is x-y. both should value 50.
 
-        double difference = x-y;
-        if (difference >= 0){
+        //for quickturns
+        if (quickClock || quickCount) {
+            if(quickClock){
+
+                frontRightPower = 1;
+                backRightPower = -1;
+                frontLeftPower = -1;
+                backLeftPower = 1;
+
+            }else{
+
+                frontRightPower = -1;
+                backRightPower = 1;
+                frontLeftPower = 1;
+                backLeftPower = -1;
+
+            }
+// actual code below
+        } else {
+
+            double difference = x - y;
+            frontRightPower = y;
+            frontRightPower += difference / 2;
+            backRightPower = difference / 2;
+
+            if (frontRightPower >= backRightPower) {
+                double speed = Math.hypot(x, y) / frontRightPower;
+                frontRightPower *= speed;
+                backRightPower *= speed;
+            } else {
+                double speed = Math.hypot(x, y) / backRightPower;
+                frontRightPower *= speed;
+                backRightPower *= speed;
+            }
+
+            backLeftPower = frontRightPower;
+            frontLeftPower = backRightPower;
 
         }
 
-    }
+        frontLeft.setPower(Range.clip(frontLeftPower, -1, 1));
+        frontRight.setPower(Range.clip(frontRightPower, -1, 1));
+        backLeft.setPower(Range.clip(backLeftPower, -1, 1));
+        backRight.setPower(Range.clip(backRightPower, -1, 1));
 
+    }
     public void stop(){
 
     }
